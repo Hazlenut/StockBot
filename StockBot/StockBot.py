7d8a09110b2ed70df197e37410cfc74489b6edff
andrewlin61 <andrew.lin61@gmail.com>
2021-04-20 17:24:33 -0400

    randm

--- StockBot/StockBot.py
+++ StockBot/StockBot.py
@@ -1,3 +1,21 @@
 import robin_stocks as rs
-rs.login()
 
+rs.login(username=your_username,
+         password=your_password,
+         expiresIn=86400,
+         by_sms=True)
+
+rs.orders.order_buy_fractional_by_price(symbol,
+                                       ammountInDollars,
+                                       timeInForce='gtc',
+                                       extendedHours=False) 
+
+
+rs.orders.order_buy_fractional_by_quantity(symbol,
+                                          quantity,
+                                          timeInForce='gtc',
+                                          extendedHours=False)
+
+rs.orders.order_buy_crypto_by_price('ETH', 
+                                 1000,
+                                 timeInForce='gtc')
