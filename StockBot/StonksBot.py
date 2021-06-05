f2c2b05927b5ceddf2e0f6d4e333c24b879090c9
Hazlenut <willie.wung@gmail.com>
2021-06-05 16:46:15 -0400

    twitch bot

--- StockBot/StonksBot.py
+++ StockBot/StonksBot.py
@@ -1,14 +1,23 @@
 import os
 from twitchio.ext import commands
+from dotenv import dotenv_values
+
+dotenv_values(".env")
 
 bot = commands.Bot(
     irc_token=os.environ['TMI_TOKEN'],
     client_id=os.environ['CLIENT_ID'],
-    name=os.environ['BOT_NAME'],
+    nick=os.environ['BOT_NICK'],
     prefix=os.environ['BOT_PREFIX'],
     channels=[os.environ['CHANNEL']]
 )
 
+
+@bot.event
+async def event_ready():
+    print(f"{os.environ['BOT_NICK']} is online!")
+    ws = bot._ws
+    await ws.send_privmsg(os.environ['CHANNEL'], f"{os.environ['BOT_NICK']} has landed")
 
 
 if __name__ == "__main__":
